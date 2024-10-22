f14bd1a01c2772ccb120efa38837553b582a57c4
Mitch Andrews <mitchelljandrews@hotmail.com>
2024-10-22 20:31:13 +1100

    added button and menu bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Frame, Button, Scale, Canvas, Label, StringVar, Entry, Toplevel, messagebox, filedialog
+import tkinter as tk 
+from tkinter import Tk, Menu, Frame, Button, Scale, Canvas, Label, StringVar, Entry, Toplevel, messagebox, filedialog
 from tkinter.colorchooser import askcolor
 
 class Window:
@@ -6,6 +7,15 @@ class Window:
         self.root = root 
         self.root.title("Drawing Application")
         self.root.geometry("1200x900")
+
+        #Tool bar
+        self.toolbar = Frame(self.root, bd=1)
+
+        #Buttons 
+        button = Button(self.toolbar, text=f"Save")
+        button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        self.toolbar.pack(side=tk.TOP, fill=tk.X)
 
 if __name__ == "__main__":
     root = Tk()
