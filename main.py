08a0f1a90369876ec0d916238e8a23af81e63fee
Mitch Andrews <mitchelljandrews@hotmail.com>
2024-10-22 21:04:46 +1100

    added more buttons

--- main.py
+++ main.py
@@ -15,9 +15,31 @@ class Window:
 
         #Buttons 
         save_button = Button(self.toolbar, text=f"Save")
-        save_button.pack(side=tk.LEFT, padx=2, pady=2)
+        save_button.place(x=0, y=0, width=100, height=75)
 
-        
+        load_button = Button(self.toolbar, text=f"Load")
+        load_button.place(x=0, y=75, width=100, height=75)
+
+        draw_button = Button(self.toolbar, text=f"Draw")
+        draw_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        eraser_button = Button(self.toolbar, text=f"Eraser")
+        eraser_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        color_button = Button(self.toolbar, text=f"Color")
+        color_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        brushes_button = Button(self.toolbar, text=f"Brushes")
+        brushes_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        shape_button = Button(self.toolbar, text=f"Shapes")
+        shape_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        undo_button = Button(self.toolbar, text=f"Undo")
+        undo_button.pack(side=tk.LEFT, padx=2, pady=2)
+
+        redo_button = Button(self.toolbar, text=f"redo")
+        redo_button.pack(side=tk.LEFT, padx=2, pady=2)
 
 if __name__ == "__main__":
     root = Tk()
