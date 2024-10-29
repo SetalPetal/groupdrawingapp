e64a43fe82eef5bd3389ec1f0d174071b9ad358c
Mitch Andrews <mitchelljandrews@hotmail.com>
2024-10-29 21:26:23 +1100

    added eraser_tool

---
+++ tools/eraser_tool.py
@@ -0,0 +1,27 @@
+import tkinter as tk 
+
+class Eraser:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.old_x = None
+        self.old_y = None
+        self.eraser_size = 10
+
+        #Mouse bindings 
+        self.canvas.bind('<B1-motion>', self.erase)
+        self.canvas.bind('<ButtonRelease-1>', self.reset)
+
+    def set_eraser_size(self, size):
+        self.eraser_size = size
+
+    def erase(self, event):
+        if self.old_x and self.old_y:
+            self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
+                                    width=self.eraser_size, fill='white',
+                                    capstyle=tk.ROUND, smooth=tk.TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x = None
+        self.old_y = None
